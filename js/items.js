7d349ea0e816bad4300684be01795df104db820f
s-stedmanj <s-stedmanj@52S500041357.it.bsd405.org>
2020-02-10 08:36:48 -0800

    test3

--- js/items.js
+++ js/items.js
@@ -1,61 +1,81 @@
+let blogs;
+let blogObjs;
 
-function CreateTableFromJSON() {
-	var myBooks = [
-            {
-                "Book ID": "1",
-                "Book Name": "Computer Architecture",
-                "Category": "Computers",
-                "Price": "125.60"
-            },
-            {
-                "Book ID": "2",
-                "Book Name": "Asp.Net 4 Blue Book",
-                "Category": "Programming",
-                "Price": "56.00"
-            },
-            {
-                "Book ID": "3",
-                "Book Name": "Popular Science",
-                "Category": "Science",
-                "Price": "210.40"
+whenLoaded = () => {
+  fetch("https://oroarmor.github.io/blog/blogIndex.json")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      blogs = data.blogs;
+    })
+    .then(() => {
+      blogObjs = [];
+      for (blog in blogs) {
+        blogObjs.push(new Blog(blogs[blog]));
+      }
+
+      let splitURL = location.href.split("?");
+      console.log(splitURL);
+      if (splitURL.length > 1) {
+        if (splitURL[1].substring(0, 5) == "blog=") {
+          console.log("blog");
+          let found = false;
+          for (let i = 0; i < blogObjs.length; i++) {
+            if (blogObjs[i].id == splitURL[1].substring(5)) {
+              blogObjs[i].addBlogTo("#blogs", {
+                class: "blog",
+              });
+              found = true;
+              break;
             }
-        ]
-   var col = [];
-        for (var i = 0; i < myBooks.length; i++) {
-            for (var key in myBooks[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
+          }
+
+          if (!found) {
+            $("#blogs").append("<p>The requested blog was not found</p>");
+            $("#prev").css("display", "none");
+            $("#next").css("display", "none");
+          } else {
+            $("#prev").attr("href", "?blog=" + ((splitURL[1].substring(5) > 0 && found) ? (splitURL[1].substring(5) - 1) : splitURL[1].substring(5)));
+            $("#next").attr("href", "?blog=" + ((splitURL[1].substring(5) < blogObjs.length - 1 && found) ? (parseInt(splitURL[1].substring(5)) + 1) : splitURL[1].substring(5)));
+          }
+        } else if (splitURL[1].substring(0, 10) == "blogStart=") {
+          let start = parseInt(splitURL[1].substring(10));
+          let end = Math.max(start - 25, 0);
+          if (splitURL[2] && splitURL[2].substring(0, 8) == "blogEnd=") {
+            end = parseInt(splitURL[2].substring(8));
+          }
+          $("#prev").css("display", "none");
+          $("#next").css("display", "none");
+          for (let i = 0; i < blogObjs.length; i++) {
+            if (blogObjs[i].id <= end && blogObjs[i].id >= start) {
+              blogObjs[i].addBlogTo("#blogs", {
+                class: "blog",
+                style: "order:" + (i * 1)
+              });
             }
+          }
+        }
+      } else { // default
+        let start = blogObjs.length - 1;
+        let end = blogObjs.length - 26;
+        for (let i = 0; i < blogObjs.length; i++) {
+          if (blogObjs[i].id >= end && blogObjs[i].id <= start) {
+            blogObjs[i].addBlogTo("#blogs", {
+              class: "blog",
+              style: "order:" + (i * 1)
+            });
+          }
         }
 
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
+        $("#prev").attr("href", "?blogStart=" + ((end < 0) ? Math.max((end - 25), 0) : end));
+        $("#next").attr("href", "?blogStart=" + ((start > blogObjs.length - 1) ? Math.min((start + 25), blogObjs.length - 1) : start));
 
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-        var tr = table.insertRow(-1);                   // TABLE ROW.
-
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
-
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < myBooks.length; i++) {
-
-            tr = table.insertRow(-1);
-
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = myBooks[i][col[j]];
-            }
-        }
-
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
-    }
+      }
+      console.log("Finished");
+    })
+    .catch(err => {
+      let errorMessage = "Failed to get blogs. " + err;
+      $("#main").append("<p>" + errorMessage + "</p>");
+    });
 };
